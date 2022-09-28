21cfd562f8a6df58c426fc7e4a4c87c0900d2a39
Jared Okawa <jokawa329@gmail.com>
2022-09-28 11:53:16 -0700

    intial commit

---
+++ Syntax Sugar Exercise/Syntax Sugar Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Syntax_Sugar_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
